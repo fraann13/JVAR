64992f38fdecf511229475017673e08fd228dc32
fraann13 <garcia.garrido.franco@gmail.com>
2025-11-24 21:38:33 -0600

    mooooore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
-    // La sintaxis 'process.env as any' debería ser reemplazada por el valor durante el build
-    parseAppId: (process.env as any)['NG_APP_PARSE_APP_ID'],
-    parseJsKey: (process.env as any)['NG_APP_PARSE_JS_KEY'],
-    parseServerUrl: (process.env as any)['NG_APP_PARSE_URL']
+    parseAppId: (process.env as any).NG_APP_PARSE_APP_ID,
+    parseJsKey: (process.env as any).NG_APP_PARSE_JS_KEY,
+    parseServerUrl: (process.env as any).NG_APP_PARSE_URL
 };
